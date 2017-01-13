2773635ed495243148c58b1d931f1866135eff54
Karanbir Singh <kbsingh@karan.org>
2017-01-13 23:18:18 +0000

    get the main run command in

--- run_che.sh
+++ run_che.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
 # if it builds, lets run it locally
+# XXX: we need docker-latest ( since base docker in CentOS
+#      is still docker-1.10
+
+mkdir -p ~/.che_root/
+docker run -it -v /var/run/docker.sock:/var/run/docker.sock \
+           -v ~/.che_root/:/data \
+            eclipse/che start
+
