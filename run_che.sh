9a9200c6fbb8fe426f77bfb59140db535663090e
Karanbir Singh <kbsingh@karan.org>
2017-01-13 23:37:35 +0000

    lets use the long form for the run script

--- run_che.sh
+++ run_che.sh
@@ -4,8 +4,21 @@
 # XXX: we need docker-latest ( since base docker in CentOS
 #      is still docker-1.10
 
-mkdir -p ~/.che_root/
-docker run -it -v /var/run/docker.sock:/var/run/docker.sock \
-           -v ~/.che_root/:/data \
-            eclipse/che start
+DataDir=~/.che_data/
+mkdir -p ${DataDir}/lib
+mkdir -p ${DataDir}/workspaces
+mkdir -p ${DataDir}/storage
+
+
+#docker run -it -v /var/run/docker.sock:/var/run/docker.sock \
+#           -v ~/.che_root/:/data \
+#            eclipse/che start
+
+docker run --net=host \
+             --name che \
+             -v /var/run/docker.sock:/var/run/docker.sock \
+             -v ${DataDir}/lib:/home/user/che/lib-copy \
+             -v ${DataDir}/workspaces:/home/user/che/workspaces \
+             -v ${DataDir}/storage:/home/user/che/storage \
+             myeclipse/che-server
 
