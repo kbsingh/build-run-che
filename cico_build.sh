e55ad36ba1829f30f52641c0f2e778641484c629
Mario Loriedo <mario.loriedo@gmail.com>
2017-01-16 18:08:45 +0100

    Che build update to be able run it as root
    Fixed a couple of things:
    - Folder $NPM_CONFIG_PREFIX must exist and should be created before running npm install
    - To be able to run bower as root `{ "allow_root": true}` must be added to `~/.bowerrc`

--- cico_build.sh
+++ cico_build.sh
@@ -10,9 +10,11 @@ yum -y install centos-release-scl java-1.8.0-openjdk-devel git patch bzip2 golan
 yum -y install rh-maven33 rh-nodejs4
 git clone https://github.com/eclipse/che
 cd che 
-export NPM_CONFIG_PREFIX=~/.che_node_modules 
+export NPM_CONFIG_PREFIX=~/.che_node_modules
 export PATH=$NPM_CONFIG_PREFIX/bin:$PATH
-scl enable rh-nodejs4 'npm install --unsafe-perm -g bower gulp typings'
+mkdir $NPM_CONFIG_PREFIX
+echo "{ \"allow_root\": true }" > ~/.bowerrc
+scl enable rh-nodejs4 'npm install -g bower gulp typings'
 scl enable rh-maven33 rh-nodejs4 'mvn clean install -Pfast'
 if [ $? -eq 0 ]; then
   # Now lets build the local docker image
