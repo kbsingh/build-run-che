cb27a98f7ca20d50d02bf632502cfb4897c8103d
Karanbir Singh <kbsingh@karan.org>
2017-01-25 08:30:54 +0000

    split out the config bits, so we can have multiple scripts consume it split out the actual build, so we can run that as a user, not root

---
+++ build_che.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# this script downloads the src and runs the build
+# to create the che binaries
+
+
+git clone -b ${GIT_BRANCH} ${GIT_REPO}
+cd che 
+mkdir $NPM_CONFIG_PREFIX
+scl enable rh-nodejs4 'npm install -g bower gulp typings'
+scl enable rh-maven33 rh-nodejs4 'mvn clean install -Pfast'
