cb4db5b30487149ff15bf90c73e7b0caaeb8ac10
Karanbir Singh <kbsingh@karan.org>
2017-02-09 17:03:02 +0000

    run che locally after building it

--- run_che.sh
+++ run_che.sh
@@ -1,24 +1,16 @@
 #!/bin/bash
 
 # if it builds, lets run it locally
-# XXX: we need docker-latest ( since base docker in CentOS
-#      is still docker-1.10
 
-DataDir=~/.che_data/
+DataDir=~/.che/
 mkdir -p ${DataDir}/lib
 mkdir -p ${DataDir}/workspaces
 mkdir -p ${DataDir}/storage
 
-
-#docker run -it -v /var/run/docker.sock:/var/run/docker.sock \
-#           -v ~/.che_root/:/data \
-#            eclipse/che start
-
-docker run --net=host \
-             --name che \
-             -v /var/run/docker.sock:/var/run/docker.sock \
-             -v ${DataDir}/lib:/home/user/che/lib-copy \
-             -v ${DataDir}/workspaces:/home/user/che/workspaces \
-             -v ${DataDir}/storage:/home/user/che/storage \
-             myeclipse/che-server
-
+docker run -d \
+	    -p 8080:8080 \
+	    --name che \
+	    -v /var/run/docker.sock:/var/run/docker.sock \
+	    -v ~/.che:/data:Z \
+	    --security-opt label:disable \
+	    rhche/che-server:nightly
