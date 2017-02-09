ddae822457720232ec5bd5d67b83b2486806b374
Jean-Francois Chevrette <jf.cron0@gmail.com>
2017-02-09 12:59:03 -0500

    display message with the che url after running it

--- run_che.sh
+++ run_che.sh
@@ -8,3 +8,6 @@ mkdir -p ${DataDir}/workspaces
 mkdir -p ${DataDir}/storage
 
 docker run -d -p 8080:8080 --name che -v /var/run/docker.sock:/var/run/docker.sock -v ~/.che:/data:Z --security-opt label:disable rhche/che-server:nightly
+
+echo -n "Che is available at http://"
+docker port che 8080/tcp
