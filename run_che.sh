b74c787c271e6e20d437a1dfcabedba33ca5b70d
Karanbir Singh <kbsingh@karan.org>
2017-02-09 17:14:40 +0000

    remove whitespaces

--- run_che.sh
+++ run_che.sh
@@ -7,10 +7,4 @@ mkdir -p ${DataDir}/lib
 mkdir -p ${DataDir}/workspaces
 mkdir -p ${DataDir}/storage
 
-docker run -d \
-	    -p 8080:8080 \
-	    --name che \
-	    -v /var/run/docker.sock:/var/run/docker.sock \
-	    -v ~/.che:/data:Z \
-	    --security-opt label:disable \
-	    rhche/che-server:nightly
+docker run -d -p 8080:8080 --name che -v /var/run/docker.sock:/var/run/docker.sock -v ~/.che:/data:Z --security-opt label:disable rhche/che-server:nightly
