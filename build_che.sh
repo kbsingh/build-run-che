f4d82e5e17954353eedea43752df3ee5ec012139
Mario Loriedo <mario.loriedo@gmail.com>
2017-03-06 23:27:09 +0100

    Build che-dependencies too

--- build_che.sh
+++ build_che.sh
@@ -5,6 +5,15 @@
 
 . config 
 
+# First build maven parent project that holds
+# versions of all Che dependencies
+# This is needed when we need to upgrate a
+# dependency and we can't wait until the CQ is
+# approved and PR is merged in upstream master branch 
+git clone -b ${GIT_BRANCH_DEP} ${GIT_REPO_DEP}
+cd che-dependencies
+scl enable rh-maven33 'mvn -B clean install'
+
 git clone -b ${GIT_BRANCH} ${GIT_REPO}
 cd che 
 mkdir $NPM_CONFIG_PREFIX
