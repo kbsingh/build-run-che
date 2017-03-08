cb81ec090ffca309761aa504b837b4abedace448
Mario Loriedo <mario.loriedo@gmail.com>
2017-03-08 12:10:56 +0100

    Use `cat` to do stacks.json replacement

--- build_che.sh
+++ build_che.sh
@@ -18,7 +18,8 @@ cd ..
 git clone -b ${GIT_BRANCH} ${GIT_REPO}
 cd che
 # Use our modified stacks
-mv ./ide/che-core-ide-stacks/src/main/resources/stacks.json.centos \ 
- ./ide/che-core-ide-stacks/src/main/resources/stacks.json
+cd ./ide/che-core-ide-stacks/src/main/resources/
+cat stacks.json.centos > stacks.json
+cd ./../../../../../
 mkdir $NPM_CONFIG_PREFIX
 scl enable rh-maven33 rh-nodejs4 'mvn -B clean install -Pfast'
