cf288cf555fc2689aad9c060b0a7c56f972bb531
Mario Loriedo <mario.loriedo@gmail.com>
2017-03-11 03:12:35 +0100

    New templace for Che deployment

--- cico_deploy.sh
+++ cico_deploy.sh
@@ -20,21 +20,14 @@ set +x
 for CLUSTER in $CHE_CLUSTERS; do 
   echo "Deploying to Cluster " $CLUSTER
   CHE_OPENSHIFT_ENDPOINT=$(eval echo \${CHE_${CLUSTER}_ENDPOINT})
-  CHE_OPENSHIFT_USERNAME=$(eval echo \${CHE_${CLUSTER}_USERNAME})
-  CHE_OPENSHIFT_PASSWORD=$(eval echo \${CHE_${CLUSTER}_PASSWORD})
   CHE_OPENSHIFT_PROJECT=$(eval echo \${CHE_${CLUSTER}_PROJECT})
-  CHE_OPENSHIFT_IMAGE=$(eval echo \${CHE_${CLUSTER}_IMAGE})
-  CHE_OPENSHIFT_SERVICEACCOUNTNAME=$(eval echo \${CHE_${CLUSTER}_SERVICEACCOUNT})
   CHE_OPENSHIFT_HOSTNAME=$(eval echo \${CHE_${CLUSTER}_HOSTNAME})
-  CHE_APPLICATION_NAME=$(eval echo \${CHE_${CLUSTER}_APPLICATION_NAME})
+  CHE_APPLICATION_NAME=che
 
   # Login
   oc login "${CHE_OPENSHIFT_ENDPOINT}" -u "${CHE_OPENSHIFT_USERNAME}" -p "${CHE_OPENSHIFT_PASSWORD}" --insecure-skip-tls-verify
   # Ensure we're in the che project
   oc project ${CHE_OPENSHIFT_PROJECT}
-
-  # Create or update template
-  oc -n ${CHE_OPENSHIFT_PROJECT} create -f che.yaml >/dev/null 2>&1 || oc -n ${CHE_OPENSHIFT_PROJECT} replace -f che.yaml >/dev/null 2>&1
 
   # Check if deploymentConfig is already present
   OUT=$(oc -n ${CHE_OPENSHIFT_PROJECT} get dc ${CHE_APPLICATION_NAME} 2> /dev/null || true)
@@ -45,19 +38,20 @@ for CLUSTER in $CHE_CLUSTERS; do
   fi
 
   # Deploy che from the template
-  oc -n ${CHE_OPENSHIFT_PROJECT} new-app --template=eclipse-che \
-    --param=APPLICATION_NAME=${CHE_APPLICATION_NAME} \
-    --param=CHE_SERVER_DOCKER_IMAGE=${CHE_OPENSHIFT_IMAGE} \
-    --param=CHE_OPENSHIFT_ENDPOINT=${CHE_OPENSHIFT_ENDPOINT} \
-    --param=CHE_OPENSHIFT_USERNAME=${CHE_OPENSHIFT_USERNAME} \
-    --param=CHE_OPENSHIFT_PASSWORD=${CHE_OPENSHIFT_PASSWORD} \
-    --param=CHE_OPENSHIFT_SERVICEACCOUNTNAME=${CHE_OPENSHIFT_SERVICEACCOUNTNAME} \
-    --param=HOSTNAME_HTTP=${CHE_OPENSHIFT_HOSTNAME} \
-    --param=CHE_LOG_LEVEL=INFO > /dev/null 2>&1
+  # oc adm policy add-scc-to-group anyuid system:authenticated or at least
+  # oc adm policy add-scc-to-group nonrot system:authenticated
+  cat che.yaml | \
+      sed "s/    hostname-http:.*/    hostname-http: ${CHE_OPENSHIFT_HOSTNAME}/" | \
+      sed "s/<route-host>/${CHE_OPENSHIFT_HOSTNAME}/" | \
+      oc apply -f -
   if [ $? -eq 0 ]; then 
     echo 'Deploy succeeded'
   else 
     echo 'Deploy Failed!'
     exit 1
   fi
+
+  # Create route
+  oc expose service che-host --hostname=${CHE_OPENSHIFT_HOSTNAME}
+
 done
