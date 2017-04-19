132f6b5af27e126a8deec796a2d9d2fc7e2f598a
David Festal <dfestal@redhat.com>
2017-04-19 20:37:22 +0200

    wrong maven command line (incorrect additional `-D` option)
    Signed-off-by: David Festal <dfestal@redhat.com>

--- build_che.sh
+++ build_che.sh
@@ -9,8 +9,8 @@ git clone -b ${RH_CHE_GIT_BRANCH} ${RH_CHE_GIT_REPO}
 
 cd rh-che
 mkdir $NPM_CONFIG_PREFIX
-scl enable rh-maven33 rh-nodejs4 'mvn -D -B clean install -U'
+scl enable rh-maven33 rh-nodejs4 'mvn -B clean install -U'
 cp target/export/che-dependencies/che/assembly/assembly-main/target/eclipse-ide-*.tar.gz ${HomeDir}
 cp assembly/assembly-main/target/eclipse-ide-*.tar.gz ${HomeDir}
-scl enable rh-maven33 rh-nodejs4 'mvn -D -B mvn --activate-profiles=-checkout-base-che -DwithoutDashboard clean install -U'
+scl enable rh-maven33 rh-nodejs4 'mvn -B mvn --activate-profiles=-checkout-base-che -DwithoutDashboard clean install -U'
 cp assembly/assembly-main/target/eclipse-ide-*.tar.gz ${HomeDir}
