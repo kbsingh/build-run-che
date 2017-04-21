6f0c2a251b86278721376c40e29cd887dd0a3f0a
David Festal <dfestal@redhat.com>
2017-04-21 20:50:13 +0200

    for the moment disable the Keycloak integration
    Signed-off-by: David Festal <dfestal@redhat.com>

--- build_che.sh
+++ build_che.sh
@@ -9,13 +9,13 @@ git clone -b ${RH_CHE_GIT_BRANCH} ${RH_CHE_GIT_REPO}
 
 cd rh-che
 mkdir $NPM_CONFIG_PREFIX
-scl enable rh-maven33 rh-nodejs4 'mvn -B install -U'
+scl enable rh-maven33 rh-nodejs4 'mvn -B -DwithoutKeycloak install -U'
 if [ $? -ne 0 ]; then
   exit $?;
 fi
 cp target/export/che-dependencies/che/assembly/assembly-main/target/eclipse-che-*.tar.gz ${HomeDir}
 cp assembly/assembly-main/target/eclipse-che-*.tar.gz ${HomeDir}
-scl enable rh-maven33 rh-nodejs4 'mvn -B --activate-profiles=-checkout-base-che -DwithoutDashboard clean install -U'
+scl enable rh-maven33 rh-nodejs4 'mvn -B --activate-profiles=-checkout-base-che -DwithoutKeycloak -DwithoutDashboard clean install -U'
 if [ $? -ne 0 ]; then
   exit $?;
 fi
