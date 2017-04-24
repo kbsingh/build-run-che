866a61a675a4fec019f39ce1850e9a723beb579c
David Festal <dfestal@redhat.com>
2017-04-24 15:30:22 +0200

    Didn't return build error code correctly
    Signed-off-by: David Festal <dfestal@redhat.com>

--- build_che.sh
+++ build_che.sh
@@ -11,12 +11,14 @@ cd rh-che
 mkdir $NPM_CONFIG_PREFIX
 scl enable rh-maven33 rh-nodejs4 'mvn -B -DwithoutKeycloak install -U'
 if [ $? -ne 0 ]; then
-  exit $?;
+  echo "Error building che/rh-che with dashboard"
+  exit 1;
 fi
 cp target/export/che-dependencies/che/assembly/assembly-main/target/eclipse-che-*.tar.gz ${HomeDir}
 cp assembly/assembly-main/target/eclipse-che-*.tar.gz ${HomeDir}
 scl enable rh-maven33 rh-nodejs4 'mvn -B --activate-profiles=-checkout-base-che -DwithoutKeycloak -DwithoutDashboard clean install -U'
 if [ $? -ne 0 ]; then
-  exit $?;
+  echo "Error building che/rh-che without dashboard"
+  exit 1;
 fi
 cp assembly/assembly-main/target/eclipse-che-*.tar.gz ${HomeDir}
