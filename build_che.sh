4548b80949608714f891237e442d5e4208578c2f
Mario Loriedo <mario.loriedo@gmail.com>
2017-04-27 19:08:52 +0200

    Enable Keycloak

--- build_che.sh
+++ build_che.sh
@@ -9,14 +9,14 @@ git clone -b ${RH_CHE_GIT_BRANCH} ${RH_CHE_GIT_REPO}
 
 cd rh-che
 mkdir $NPM_CONFIG_PREFIX
-scl enable rh-maven33 rh-nodejs4 'mvn -B -DwithoutKeycloak install -U'
+scl enable rh-maven33 rh-nodejs4 'mvn -B install -U'
 if [ $? -ne 0 ]; then
   echo "Error building che/rh-che with dashboard"
   exit 1;
 fi
 cp target/export/che-dependencies/che/assembly/assembly-main/target/eclipse-che-*.tar.gz ${HomeDir}
 cp assembly/assembly-main/target/eclipse-che-*.tar.gz ${HomeDir}
-scl enable rh-maven33 rh-nodejs4 'mvn -B --activate-profiles=-checkout-base-che -DwithoutKeycloak -DwithoutDashboard clean install -U'
+scl enable rh-maven33 rh-nodejs4 'mvn -B --activate-profiles=-checkout-base-che -DwithoutDashboard clean install -U'
 if [ $? -ne 0 ]; then
   echo "Error building che/rh-che without dashboard"
   exit 1;
