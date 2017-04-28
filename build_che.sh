d8f65d28fcbae85a7629cb2218dc3fa07fc2672f
David Festal <dfestal@redhat.com>
2017-04-28 11:17:39 +0200

    Provide the ability to skip modules of the RH distribution build
    https://issues.jboss.org/browse/CHE-241
    
    Signed-off-by: David Festal <dfestal@redhat.com>

--- build_che.sh
+++ build_che.sh
@@ -15,10 +15,11 @@ if [ $? -ne 0 ]; then
   exit 1;
 fi
 cp target/export/che-dependencies/che/assembly/assembly-main/target/eclipse-che-*.tar.gz ${HomeDir}
-cp assembly/assembly-main/target/eclipse-che-*.tar.gz ${HomeDir}
+cp target/builds/fabric8/fabric8-che/assembly/assembly-main/target/eclipse-che-*.tar.gz ${HomeDir}
+
 scl enable rh-maven33 rh-nodejs4 'mvn -B --activate-profiles=-checkout-base-che -DwithoutDashboard clean install -U'
 if [ $? -ne 0 ]; then
   echo "Error building che/rh-che without dashboard"
   exit 1;
 fi
-cp assembly/assembly-main/target/eclipse-che-*.tar.gz ${HomeDir}
+cp target/builds/fabric8-without-dashboard/fabric8-che/assembly/assembly-main/target/eclipse-che-*.tar.gz ${HomeDir}
